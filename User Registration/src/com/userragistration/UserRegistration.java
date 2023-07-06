aaa6f2d45a0b18eabde5e369a8e53f6d27cf7c4c
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-07-06 16:17:35 +0530

    Valid Last Name

--- User Registration/src/com/userragistration/UserRegistration.java
+++ User Registration/src/com/userragistration/UserRegistration.java
@@ -19,9 +19,23 @@ public class UserRegistration {
         }
 
     }
+    public void validLastName(){
+        Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Last Name");
+        String lastName=sc.nextLine();
+
+        Matcher matcher=pattern.matcher(lastName);
+        if (matcher.matches()){
+            System.out.println("Last Name is Valid");
+        }else {
+            System.out.println("Last Name is not valid");
+        }
+    }
 
     public static void main(String[] args) {
         UserRegistration ur = new UserRegistration();
         ur.validFirstName();
+        ur.validLastName();
     }
 }
